d36ed2d8db41b90b210d6510f5eeee4bb22ec766
Akash <akashsuper2000@gmail.com>
2019-09-05 21:22:42 +0530

    front-end final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,223 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class User extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [{name: 'Soup', price: 200}, {name: 'Cake', price: 100}],
+      cost: 0
+    }
+  };
+
+  componentDidMount() {
+      let self = this;
+      fetch('/items', {
+          method: 'POST'
+      }).then(function(response) {
+          if (response.status >= 400) {
+              throw new Error("Bad response from server");
+          }
+          return response.json();
+      }).then(function(data) {
+          self.setState({items: data});
+      }).catch(err => {
+      console.log('Caught it!',err);
+      })
+    }
+
+  handleSelect = (event) => {
+    event.preventDefault();
+    var total = 0, i;
+    for(i=0; i<document.getElementsByName('name').length; i++){
+      total+=document.getElementsByName('quantity')[i].value*document.getElementsByName('price')[i].innerHTML;
+    }
+    this.setState({cost: total});
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    var data = {cost: this.state.cost};
+    console.log(JSON.stringify(data));
+    fetch("/bill", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    }).then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+    }).then(function(data) {
+        console.log(data);  
+        // eslint-disable-next-line
+        if(data == "success"){
+        console.log('Success'); 
+        }
+    }).catch(function(err) {
+        console.log(err);
+    });
+  }
+
+  render(){
+    return(
+
+      <div className='Container'>
+
+          <h1 className='Heads'>Restaurant Menu</h1>
+          <div className='Content'>
+
+            <form onSubmit={this.handleSubmit}>
+              
+              {this.state.items.map((item,i) =>
+                <div className='FormGroup' key={i}>
+                  <span className='Subheads' name='name'>{item.name}</span>
+                  <span className='Price' name='price'>{item.price}</span>
+                  <input className='Boxes' type='number' name='quantity' onChange={this.handleSelect}></input>
+                </div>
+              )}
+              
+              <p className='Subheads' style={{display: 'block'}}>Total cost: {this.state.cost}</p>
+              <input className='btn-lg btn btn-default' type='submit' value='Order'/>
+          
+            </form>
+          </div>
+        </div>
+
+    );
+  }
+
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+class Admin extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [{name: 'Soup', price: 200}, {name: 'Cake', price: 100}]
+    }
+  };
+
+  componentDidMount() {
+    document.getElementById("addbox").style.display = "none";
+      let self = this;
+      fetch('/items', {
+          method: 'POST'
+      }).then(function(response) {
+          if (response.status >= 400) {
+              throw new Error("Bad response from server");
+          }
+          return response.json();
+      }).then(function(data) {
+          self.setState({items: data});
+      }).catch(err => {
+      console.log('Caught it!',err);
+      })
+    }
+
+  handleSelect = (event) => {
+    event.preventDefault();
+    document.getElementById("addbox").style.display = "none";
+    this.state.items.push({name: document.getElementsByName('newname')[0].value, price: document.getElementsByName('newprice')[0].value});
+    this.setState({items: this.state.items});
+  }
+
+  handleAdd = (event) => {
+    event.preventDefault();
+    document.getElementById("addbox").style.display = "block";
+  }
+
+  handleRemove = (event) => {
+    event.preventDefault();
+    var removeByAttr = function(arr, attr, value){
+    var i = arr.length;
+    while(i--){
+       if( arr[i] 
+           && arr[i].hasOwnProperty(attr) 
+           && (arguments.length > 2 && arr[i][attr] === value ) ){ 
+
+           arr.splice(i,1);
+
+       }
+    }
+    return arr;
+    }
+
+    removeByAttr(this.state.items, 'name', event.target.previousSibling.previousSibling.innerHTML);
+    this.setState({items: this.state.items});
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(JSON.stringify(this.state.items));
+    fetch("/update", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(this.state.items)
+    }).then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+        // eslint-disable-next-line
+        if(data == "success"){
+        console.log('Success'); 
+        }
+    }).catch(function(err) {
+        console.log(err);
+    });
+  }
+
+  render(){
+    return(
+
+      <div className='Container'>
+
+        <h1 className='Heads'>Admin</h1>
+        <div className='Content'>
+
+        <form onSubmit={this.handleSubmit}>
+          
+          {this.state.items.map((item,i) =>
+          <div className='FormGroup' key={i}>
+            <span className='Subheads' name='name'>{item.name}</span>
+            <span className='Price' name='price'>{item.price}</span>
+            <input className='btn btn-lg btn-default' style={{margin: 10, width: 200, padding: 5}} type='button' value='- Remove item' onClick={this.handleRemove}></input>
+          </div>
+          )}
+
+          <form onSubmit={this.handleSelect}>
+          <div className='FormGroup' id='addbox'>
+            <input className='Boxes' style={{margin: 10, width: 200, padding: 5}} name='newname' type='text'></input>
+            <input className='Boxes' style={{margin: 10, width: 100, padding: 5}} name='newprice' type='number'></input>
+            <input className='btn-lg btn btn-default' type='submit' value='Add'/>
+          </div>
+          </form>
+          
+          <input className='btn-lg btn btn-default' type='button' value='+ Add an item' onClick={this.handleAdd} style={{display: 'block', marginTop: 20, marginBottom: 20}}></input>
+          <input className='btn-lg btn btn-default' type='submit' value='Update'/>
+      
+        </form>
+        </div>
+      </div>
+
+    );
+  }
+
+}
+
+export {Admin, User};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './App.css';
+import './bootstrap.css';
+import {Admin, User} from './App';
 import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isLoggedIn: 0};
+  }
+
+  handleAdmin = (event) =>{
+  	this.setState({isLoggedIn: 1});
+  }
+
+  handleUser = (event) =>{
+  	this.setState({isLoggedIn: 2});
+  }
+
+  handleBack = (event) =>{
+  	this.setState({isLoggedIn: 0});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+
+    if (isLoggedIn===1) {
+      return (
+      	<div>
+      	<input className='btn-lg btn btn-default' style={{display: 'inlineBlock'}} type='button' value='<' onClick={this.handleBack}></input>
+      	<Admin />
+      	</div>
+      	);
+    } else if (isLoggedIn===2) {
+      return (
+      	<div>
+      	<input className='btn-lg btn btn-default' style={{display: 'inlineBlock'}} type='button' value='<' onClick={this.handleBack}></input>
+      	<User />
+      	</div>
+      	
+      	);
+    }
+    else{
+    return (
+    <div>
+    	<input className='btn-lg btn btn-default' style={{display: 'inlineBlock'}} type='button' value='<' onClick={this.handleBack}></input>
+      <div className='Container'>
+
+          <h1 className='Heads'>Login As</h1>
+          <div className='Content'>
+          <form>
+          <div className='FormGroup'>
+          <input className='btn-lg btn btn-default Butn Select' type='submit' value='Admin' onClick={this.handleAdmin}></input>
+          </div>
+
+          <div className='FormGroup'>
+          <input className='btn-lg btn btn-default Butn Select' type='submit' value='User' onClick={this.handleUser}></input>
+          </div>
+          </form>       
+              
+          </div>
+        </div>
+     </div>
+    );
+	}
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
